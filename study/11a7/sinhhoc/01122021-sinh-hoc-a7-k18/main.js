3805685fa708ef6692a8c20fe9eedc043a9d9cc3
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-01 09:02:26 +0700

    Update main.js

--- study/11a7/sinhhoc/01122021-sinh-hoc-a7-k18/main.js
+++ study/11a7/sinhhoc/01122021-sinh-hoc-a7-k18/main.js
@@ -1,1 +1,52 @@
-
+var dataInner = [
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+  {"dd":"",
+   "c3":"",
+   "c4":"",
+   "cam":"",
+  },
+]
+let innerTable = "";
+for (let i=0; i<innerData.length;i++){
+  innerTable += `
+  <td>${dataInner[i].dd}</td>
+      <td>${dataInner[i].c3}</td>
+      <td>${dataInner[i].c4}</td>
+      <td>${dataInner[i].cam}</td>
+  `;
+}
+document.getElementById('innerData').innerHTML += innerTable
