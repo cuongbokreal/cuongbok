11c85202d64316b3d09e45280431f50aa417062a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-09 13:35:41 +0700

    Create main.py

---
+++ documents/code/python/AiLaTrieuPhu/main.py
@@ -0,0 +1,24 @@
+<head>
+  <meta charset="UTF-8">
+</head>
+
+#Ai là Free Fire by Cuongbok
+listQuest = [
+    {"quest":"1+1=?",
+     "listAnswer":["1","2","3","4"],
+     "true":"B"
+    },
+    {"quest":"2+2=?",
+     "listAnswer":["1","2","3","4"],
+     "true":"D"
+    },
+    ]
+null_name = 'Giả lập Cuongbok'
+import base64, codecs
+magic = 'cGxheWVyX25hbWUgPSBpbnB1dCgnTmjhuq1wIHTDqm4gY+G7p2EgYuG6oW4nKQppZiBsZW4ocGxheWVyX25hbWUpID09IDA6CiAgICBwcmludChmJ0LhuqFuIGNoxrBhIG5o4bqtcCB0w6puLCBuw6puIG3DrG5oIMSR4bq3dCBsw6Age251bGxfbmFtZX0gbmjDqSEgY2jDumMgbWF5IG3huq9uIDopKScpCiAgICBwbGF5ZXJfbmFtZSA9IG51bGxfbmFtZQpwcmludChmJ1hpbiBjaMOgbyBkw6JuIGNoxqFpIHtwbGF5ZXJfbmFtZX0nKQpwbGF5ZXJfcG9pbnQgPSAwCmxpc3RDaG9vc2UgPSBbIkEiLCJCIiwiQyIsIkQiLCJFIiwiRiIsIkciXQpsaXN0WHVseUNob29zZSA9IHsiQSI6IjAiLCJCIjoiMSIsIkMiOiIyIiwiRCI6IjMiLCJFIjoiNCIsIkYiOiI1IiwiRyI6IjYifQogICAgICAgCmltcG9ydCByZQpmb3IgaSBpbiB'
+love = 'lLJ5aMFtjYTkyovufnKA0HKIyp3DcXGbXVPNtVUOlnJ50XTLaKT5P4oduovQRxJShMlOwj7Z6VUgjoTS5MKWspT9coaE9VZFEnrT7t20aXDbtVPNtpUWcoaDbMvqQj6W1VTwuh49cVUCuh5Rtr2xeZK0ir2kyovufnKA0HKIyp3DcsGbtr2kcp3EEqJImqSgcKIfvpKIyp3DvKK0aXDbtVPNtMz9lVTkyoxShp3qypvOcovOlLJ5aMFtjYTkyovufnKA0HKIyp3EonI1oVzkcp3EOoaA3MKVvKFxcBtbtVPNtVPNtVUOlnJ50XTLar2kcp3EQnT9ip2IooTIhDJ5mq2IlKK0hr2kcp3EEqJImqSgcKIfvoTymqRShp3qypvWqJ2kyoxShp3qypy19WljtMJ5xCFVtVPVcPvNtVPOjoTS5MKWsLJ5mq2IlVQ0tnJ5jqKDbW1khD8BvqFO0phT6blOf4ohqnGbaXDbtVPNtpTkurJIlK2Shp3qypvN9VUOfLKyypy9uoaA3MKVhpzIjoTSwMFtaLF'
+god = 'csJ0EnKS5yZXBsYWNlKCdiJywnQicpLnJlcGxhY2UoJ2MnLCdDJykucmVwbGFjZSgnZCcsJ0QnKQogICAgaWYgbGVuKHBsYXllcl9hbnN3ZXIpID09IDA6CiAgICAgICAgcGxheWVyX2Fuc3dlciA9ICdudWxsJwogICAgcmVnID0gJy9BfEJ8Q3xEfEV8RnxHL2cnCiAgICBjaGVjayA9IHJlLmZpbmRhbGwocGxheWVyX2Fuc3dlciwgcmVnKQogICAgaWYgY2hlY2s6CiAgICAgICAgdHJ1ZUFuc3dlciA9IGxpc3RRdWVzdFtpXVsidHJ1ZSJdCiAgICAgICAgaWYgbGlzdFh1bHlDaG9vc2VbcGxheWVyX2Fuc3dlcl0gPT0gbGlzdFh1bHlDaG9vc2VbdHJ1ZUFuc3dlcl06CiAgICAgICAgICAgIHBsYXllcl9wb2ludCArPSAxCiAgICAgICAgICAgIHByaW50KGYnVHLhuqMgbOG7nWkgxJHDum5nISBC4bqhbiDEkcaw4bujYyBj4'
+destiny = 'ohMozptZFQRxJauh4AgWlxXVPNtVPNtVPOyoUAyBtbtVPNtVPNtVPNtVPOjpzyhqPuzW1El4odwVTmuh51cVUAunFRtkWQQbKNtj6ShVZFEj7chMlOfj6Ntr3ElqJIOoaA3MKW9WlxXVPNtVTIfp2H6PvNtVPNtVPNtpUWcoaDbW05b4odgpPOmLJxtkWUQbKNtj6ShYPOenZB0ozptqZBgozttkWSc4ohQoFOhnTRuWlxXVPNtVTyzVTxeZFN+CFOfMJ4boTymqSS1MKA0XGbXVPNtVPNtVPOjpzyhqPuzW1khEZBvovOwnZnunFO7pTkurJIlK25uoJI9VZFEj6ZtnT/QbT4tqTwQbT5bVUgfMJ4boTymqSS1MKA0XK0tL8BvqFOb4ohCnFjtp+T7xFQRxJauh4AgVTCuh6quVTYuhdShVTmQbPO7pTkurJIlK3OinJ50sFpcPaOlnJ50XPqpowVjZwRtLaxtD3Iiozqvo2ftDGpgFmR4VPuQqJ9hM2Win2y0YPOQqJ9hM2Win3WyLJjcWlxX'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
